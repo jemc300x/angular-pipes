7fa96f3e43f03a59a997f434d57457917bb32cb0
jemc300x <jemc300x@gmail.com>
2020-04-04 08:20:49 -0500

    simplify logic

--- src/app/pipes/showpass.pipe.ts
+++ src/app/pipes/showpass.pipe.ts
@@ -6,17 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ShowpassPipe implements PipeTransform {
 
   transform(value: string, show: boolean = true): string {
-    
-    if ( show ) {
-      return value;
-    } else {
-      const valueLength = value.length;
-      let newValue = '';
-      for (let index = 0; index < valueLength; index++) {
-        newValue += '*';
-      }
-      return newValue;
-    }
+    return (show) ? value : '*'.repeat(value.length);
   }
 
 }
